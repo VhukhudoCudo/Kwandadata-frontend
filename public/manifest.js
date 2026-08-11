89532abd69a9652764427c559b6ad2daef42bdd6
KwandaLink Developer <dev@kwandalink.com>
2026-08-11 11:18:46 +0200

    fix

---
+++ public/manifest.js
@@ -0,0 +1,24 @@
+{
+  "name": "KwandaData",
+  "short_name": "KwandaData",
+  "description": "Participate. Earn. Connect.",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#14b8a6",
+  "theme_color": "#14b8a6",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "/assets/img/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "/assets/img/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
